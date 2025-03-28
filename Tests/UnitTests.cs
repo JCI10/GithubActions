4e312aa9c9658d0c39399f6a12e84f2f11d1100c
Jason Irwin <148006399+JCI10@users.noreply.github.com>
2025-03-27 20:54:14 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,7 +6,7 @@ public class Addition
 	[TestMethod]
 	public void Add_Valid_Patino()
 	{
-		Assert.AreEqual(2, Program.Add("1", "2"));
+		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
@@ -55,7 +55,7 @@ public class Addition
 	public void Multiply_Valid_Patino()
 	{
 		Assert.AreEqual(2, Program.Multiply("1", "2"));
-		Assert.AreEqual(15, Program.Multiply("3", "2"));
+		Assert.AreEqual(6, Program.Multiply("3", "2"));
 		Assert.AreEqual(35, Program.Multiply("5", "7"));
 	}
 
